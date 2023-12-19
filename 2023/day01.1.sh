e0a0a3ef5c36cdcc7106267f4b41298d5fb228d9
nitro2 <ngothanhnhannt@gmail.com>
2023-12-19 17:08:22 +0700

    Day1 part 1

--- 2023/day01.1.sh
+++ 2023/day01.1.sh
@@ -1,1 +1,1 @@
-sed 's/[^0-9]*//g' | sed 's/\(.\).*\(.\)/\1\2/' | awk '{sum += ($1 > 9 ? $1 : ($1)*11)} END {print sum}'
+sed 's/[^0-9]*//g' | sed 's/\(.\).*\(.\)/\1\2/' | awk '{s = ($1 > 9 ? $1 : ($1)*11) ; sum += s; print s} END {print sum}'
